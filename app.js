04ca659709dde248cb9dfa9e5a2da9a30216b9fa
J3ffJessie <jejess6125@gmail.com>
2019-06-26 17:51:26 -0500

    Initial commit of Newsletter Signup page.

---
+++ app.js
@@ -0,0 +1,27 @@
+//jshint esversion: 6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/", function(req,res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  
+  var firstName = req.body.firstName;
+  var lastName =  req.body.lastName;
+  var email = req.body.email;
+
+console.log(firstName, lastName, email);
+
+})
+
+app.listen(5500, function () {
+  console.log("Server is running on port 5500.")
+});
