743faf288ddc57455d071a380956667e3c84271f
J3ffJessie <jejess6125@gmail.com>
2019-06-29 17:05:40 -0500

    Added API request to Mailchimp to add subscribers. Added success and failure pages for errors and successful subscriptions.

--- app.js
+++ app.js
@@ -7,21 +7,62 @@ const request = require('request');
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended: true}));
-app.get("/", function(req,res){
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res){
-  
-  var firstName = req.body.firstName;
-  var lastName =  req.body.lastName;
-  var email = req.body.email;
+app.post("/", function (req, res) {
 
-console.log(firstName, lastName, email);
+      var firstName = req.body.firstName;
+      var lastName = req.body.lastName;
+      var email = req.body.email;
 
-})
 
-app.listen(5500, function () {
-  console.log("Server is running on port 5500.")
-});
+      var data = {
+        members: [
+          {
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+              FNAME: firstName,
+              LNAME: lastName
+            }
+          }
+          ]
+        };
+
+        var jsonData = JSON.stringify(data);
+
+
+        var options = {
+          url: 'https://us3.api.mailchimp.com/3.0/lists/7d20e4d928',
+          method: "POST",
+          headers: {
+            "Authorization": "Jeff1 fa0f0c31e37f9a09183f0eb9c56a7d15-us3"
+          },
+          body: jsonData
+        };
+        request(options, function (error, response, body) {
+          if (error) {
+            res.sendFile(__dirname + "/failure.html");
+          } else {
+            res.sendFile(__dirname + "/success.html")
+          }
+
+        });
+
+
+
+
+      });
+
+    app.listen(5500, function () {
+      console.log("Server is running on port 5500.")
+    });
+
+
+    // API Key for Mailchimp  fa0f0c31e37f9a09183f0eb9c56a7d15-us3
+    // mailchimp campaign ID  7d20e4d928
