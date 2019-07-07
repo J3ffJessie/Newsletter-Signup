71417403b67c56f2a36abe504c8a263f0ec637b2
J3ffJessie <jejess6125@gmail.com>
2019-07-07 14:16:48 -0500

    Committing changes for Heroku deployment

--- app.js
+++ app.js
@@ -16,53 +16,56 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
 
-      var firstName = req.body.firstName;
-      var lastName = req.body.lastName;
-      var email = req.body.email;
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
 
 
-      var data = {
-        members: [
-          {
-            email_address: email,
-            status: "subscribed",
-            merge_fields: {
-              FNAME: firstName,
-              LNAME: lastName
-            }
-          }
-          ]
-        };
+  var data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  };
 
-        var jsonData = JSON.stringify(data);
+  var jsonData = JSON.stringify(data);
 
 
-        var options = {
-          url: 'https://us3.api.mailchimp.com/3.0/lists/7d20e4d928',
-          method: "POST",
-          headers: {
-            "Authorization": "Jeff1 fa0f0c31e37f9a09183f0eb9c56a7d15-us3"
-          },
-          body: jsonData
-        };
-        request(options, function (error, response, body) {
-          if (error) {
-            res.sendFile(__dirname + "/failure.html");
-          } else {
-            res.sendFile(__dirname + "/success.html")
-          }
+  var options = {
+    url: 'https://us3.api.mailchimp.com/3.0/lists/7d20e4d928',
+    method: "POST",
+    headers: {
+      "Authorization": "Jeff1 bb23cea8281547d062a64f71b0eba369-us3"
+    },
+    body: jsonData
+  };
+  request(options, function (error, response, body) {
+    if (error) {
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
 
-        });
+  });
+
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 5500, function () {
+  console.log("Server is running on port 5500.")
+});
 
 
-
-
-      });
-
-    app.listen(5500, function () {
-      console.log("Server is running on port 5500.")
-    });
-
-
-    // API Key for Mailchimp  fa0f0c31e37f9a09183f0eb9c56a7d15-us3
-    // mailchimp campaign ID  7d20e4d928
+// API Key for Mailchimp  fa0f0c31e37f9a09183f0eb9c56a7d15-us3
+// mailchimp campaign ID  7d20e4d928
